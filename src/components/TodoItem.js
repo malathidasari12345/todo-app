e55afe1e20628148f5d672680c0166ff9bd21dbc
Malathi Dasari <malathidasari30@gmail.com>
2024-09-16 12:18:42 +0530

    changes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -32,7 +32,8 @@ const TodoItem = ({ todo }) => {
          :
          (
           <Stack spacing={3} alignItems="center">
-            <Typography> <b>Id:</b> {todo.id}</Typography>
+            <Typography> <b>UserId:</b> {todo.userId
+            }</Typography>
             <Typography> <b>Todo:</b> {todo.todo}</Typography>
             <Typography><b>Completed:</b> {todo.completed ? "Yes" : "No"}</Typography>
             <Stack direction="row" spacing={2}>
